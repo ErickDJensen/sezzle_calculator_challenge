032f73579cc8c1ad69bf8575dec5b75ceb009135
Erick Jensen <Erick.Jensen010@gmail.com>
2020-11-06 08:59:44 -0600

    clean up code

--- src/index.js
+++ src/index.js
@@ -2,35 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 
-//Bring in Redux Logger
-import logger from 'redux-logger';
-//Bring in Router
-import { HashRouter as Router } from 'react-router-dom';
-// Bring in Redux
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
+ReactDOM.render(<App />, document.getElementById('root'));
 
-//Reducer to store feedback state
-const feedback = (state = {feeling: 0, understanding: 0, support: 0, comments: ''}, action) => {
-    if (action.type === 'ADD_FEEDBACK') {
-        return {...state, ...action.payload}
-    }
-    //Conditional statement to reset survey data for user
-    if(action.type === 'RESET_FEEDBACK') {
-        return {feeling: 0, understanding: 0, support: 0, comments: ''}
-    }
-    return state;
-}
-
-//Create store
-const storeInstance = createStore(
-    combineReducers({
-        feedback
-    }),
-    applyMiddleware(logger) //Add our middleware logger
-);
-
-ReactDOM.render(<Provider store={storeInstance}><Router><App /></Router></Provider>, document.getElementById('root'));
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
